a71010c447cccfef9bc0001e44e4d244f163e58d
Aaron Kofsky <a2aarontothe2@gmail.com>
2018-02-09 22:26:53 -0500

    Make Euler's integrate and integrate_to methods the default implemnation

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::ops::Generator;
 fn main() {
     println!("Hello, world!");
     let funct = |t: f64, x: f64| -> f64 { t*x };
-    let mut x_gen = Euler{}.integrate_to(&funct, 1.1, 0.0, 5.06, 0.1);
+    let x_gen = Euler.integrate_to(&funct, 1.1, 0.0, 5.06, 0.1);
     for _ in 0..10 {
         println!("{:?}", x_gen);
     }
@@ -13,20 +13,6 @@ fn main() {
 
 trait Integrator {
     fn step<F: Fn(f64, f64) -> f64>(&self, f: &F, x0: f64, t0: f64, dt: f64) -> f64;
-
-    fn integrate<'a, F: Fn(f64, f64) -> f64>(&'a self, f: &'a F, x0: f64, t0: f64, dt: f64) -> Box<Generator<Return = f64, Yield = f64> + 'a>;
-
-    fn integrate_to<F: Fn(f64, f64) -> f64>(&self, f: &F, x0: f64, t0: f64, t_end: f64, dt: f64) -> f64;
-}
-
-
-struct Euler {
-}
-
-impl Integrator for Euler {
-    fn step<F: Fn(f64, f64) -> f64>(&self, f: &F, x0: f64, t0: f64, dt: f64) -> f64 {
-        x0 + dt * f(t0, x0)
-    }
 
     fn integrate<'a, F: Fn(f64, f64) -> f64>(&'a self, f: &'a F, mut x0: f64, mut t0: f64, dt: f64) -> Box<Generator<Return = f64, Yield = f64> + 'a> {
         Box::new(move || {
@@ -47,5 +33,14 @@ impl Integrator for Euler {
         // Take one last small step to land right on t_end
         let t_diff = t_end - t0;
         self.step(f, x0, t0, t_diff)
+    }
+}
+
+
+struct Euler;
+
+impl Integrator for Euler {
+    fn step<F: Fn(f64, f64) -> f64>(&self, f: &F, x0: f64, t0: f64, dt: f64) -> f64 {
+        x0 + dt * f(t0, x0)
     }
 }
